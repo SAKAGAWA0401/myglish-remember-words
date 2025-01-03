3f3b44d0ab8024bd1d83daa209083749eb35b893
SAKAGAWA0401 <rts0401@gmail.com>
2025-01-04 01:37:56 +0800

    Support both local secret file and production env variable

--- server/src/db/firebase.js
+++ server/src/db/firebase.js
@@ -1,11 +1,17 @@
 const admin = require("firebase-admin");
 const path = require("path");
-const serviceAccountKeyFile = path.resolve(process.env.FIREBASE_KEY_FILE);
+let serviceAccount;
 
-if (!serviceAccountKeyFile) {
-  throw new Error("FIREBASE_KEY_FILE is not defined in environment variables");
+if (process.env.FIREBASE_KEY) {
+  // デプロイ環境用: 環境変数から直接初期化
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else if (process.env.FIREBASE_KEY_FILE) {
+  // 開発環境用: JSON ファイルから初期化
+  const serviceAccountKeyFile = path.resolve(process.env.FIREBASE_KEY_FILE);
+  serviceAccount = require(serviceAccountKeyFile);
+} else {
+  throw new Error("FIREBASE_KEY or FIREBASE_KEY_FILE must be defined");
 }
-const serviceAccount = require(serviceAccountKeyFile);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
